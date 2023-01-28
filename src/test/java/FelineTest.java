02185a7487cafb26bac50b7a3d9e1e15980f2d38
Olga Bileva <a.niramas@yandex.ru>
2023-01-28 19:08:36 +0300

    felineTester

--- src/test/java/FelineTest.java
+++ src/test/java/FelineTest.java
@@ -3,6 +3,7 @@ import com.example.Lion;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -15,14 +16,7 @@ public class FelineTest {
     public void felineTester() throws Exception {
         Lion lion = new Lion("Самец", feline);
         lion.getFood();
-
-//        feline.getFamily();
-//        Mockito.verify(feline).getFamily();
-//        Mockito.when(feline.getFamily()).thenReturn("Кошачьи");
-//        assertEquals(feline.getFamily(), "Кошачьи");
-//
-//        Feline fel2 = new Feline();
-//        assertEquals(fel2.getFamily(), "Кошачьи");
+        Mockito.verify(feline).eatMeat();
 
     }
 }
